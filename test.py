522d7b6f45df12a838cbcc005c996d4733f983ec
Alex <xalex1995x@gmail.com>
2019-04-20 18:30:09 +0300

    descriptor

--- test.py
+++ test.py
@@ -1,15 +1,33 @@
-import file
-import tempfile
-import os
+# import file
+# import os
+#
+#
+# file1 = file.File(os.path.join('F:\\WORK\\python_sendbox', "test1.data"))
+# file1.write("""testMsg1
+# testMsg1""")
+#
+# file2 = file.File(os.path.join('F:\\WORK\\python_sendbox', "test2.data"))
+# file2.write("testMsg2")
+#
+# file3 = file1 + file2
+# for f in file3:
+#     print(f)
+from value import Value
 
 
-file1 = file.File(os.path.join('F:\\WORK\\python_sendbox', "test1.data"))
-file1.write("""testMsg1
-testMsg1""")
+class Account:
+    amount = Value()
 
-file2 = file.File(os.path.join('F:\\WORK\\python_sendbox', "test2.data"))
-file2.write("testMsg2")
+    def __init__(self, commission):
+        self.commission = commission
 
-file3 = file1 + file2
-for f in file3:
-    print(f)
+    def _get_commission(self):
+        return self.commission
+
+
+new_account = Account(0.1)
+
+
+for i in range(0, 110, 10):
+    new_account.amount = i
+    print(str(i) + " " + str(new_account.amount))
---
+++ value.py
@@ -0,0 +1,12 @@
+class Value:
+    def __init__(self):
+        self.amount = 0
+
+    def __get__(self, instance, owner):
+        return self.amount
+
+    def __set__(self, instance, value):
+        self.amount = int(value * (1 - instance.commission))
+
+
+
